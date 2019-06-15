ecec06be2fe36a60e956859265e6bb0310bd9266
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-06-15 17:15:47 +0200

    Adds basic methods

--- api/my-agreements.js
+++ api/my-agreements.js
@@ -1,5 +1,17 @@
 const { send } = require('micro')
+const withTokenAuth = require('../lib/token-auth')
+const getUserName = require('../lib/get-user-name')
+const getUid = require('../lib/get-uid-from-username')
+const getAgreements = require('../lib/get-agreements')
+const logger = require('../lib/logger')
 
-module.exports = async (request, response) => {
-  send(response, 200, 'Howdy stranger')
+const getMyAgreements = async (request, response) => {
+  const { upn } = request.token
+  const userName = getUserName(upn)
+  logger('info', ['my-agreements', 'getMyAgreements', 'upn', upn, 'userName', userName])
+  const uid = await getUid(userName)
+  const agreements = await getAgreements(uid)
+  send(response, 200, agreements)
 }
+
+module.exports = (request, response) => withTokenAuth(request, response, getMyAgreements)
